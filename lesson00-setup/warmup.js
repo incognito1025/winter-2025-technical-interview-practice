71b2b21e0187f592d46d478c8911c5bb72ba61f8
incognito1025 <nkhanamart@gmail.com>
2025-01-19 11:38:41 -0500

    Testing first commit of warmup.js in L3

--- lesson00-setup/warmup.js
+++ lesson00-setup/warmup.js
@@ -1,5 +1,13 @@
 // Write a function that introduces yourself. It should take your name and your hometown and return a string that says "Hello, my name is [your name] and I am from [your hometown]."
 
-function introduceYourself(name, hometown) {}
+function introduceYourself(name, hometown) {
+    let introduction = `Hello, my name is ${name} and I am from ${hometown}`;
+
+    return introduction;
+}
 
 // Then console.log the result of your function to the console.
+
+console.log(introduceYourself("Niki", "Brooklyn"));
+console.log(introduceYourself("John Doe", "NY"));
+console.log(introduceYourself("Jane Doe", "Oz"));
