8efaa1ce34efb85abcf7f5820744c4a5d4a3b494
incognito1025 <nkhanamart@gmail.com>
2025-01-23 05:17:39 -0500

    problem.js not passing - why?

--- lesson00-setup/problem.js
+++ lesson00-setup/problem.js
@@ -1,3 +1,20 @@
-function sayHello() {}
+function sayHello() {
+  return "Hello, World!";
+}
+
+console.log(sayHello());
 
 module.exports = sayHello;
+
+/*
+//Test is not passing. why?
+  lesson00-setup git:(main) ✗ npm run test ./lesson00-setup/problem.test.js
+
+> dsamasterclass@1.0.0 test
+> jest ./lesson00-setup/problem.test.js
+
+sh: jest: command not found
+➜  lesson00-setup git:(main) ✗
+
+
+*/
